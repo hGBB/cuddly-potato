fce999131995a9a2a4ea1d4fa7744abf787629b4
Martin Binder <martin.binder3@gmx.net>
2018-04-30 17:52:02 +0200

    first shell commit

---
+++ A/src/Shell.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public final class Shell {
+    private Shell() {}
+
+    private static void main(String[] args) {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        execute(stdin);
+    }
+
+    private static void execute(BufferedReader stdin) throws IOException {
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("prompt");
+            String input = stdin.readLine();
+            if (input == null) {
+                break;
+            }
+            Scanner scanner = new Scanner(input);
+            scanner.useDelimiter("\\s+");
+            if (scanner.hasNext()) {
+                String command = scanner.next();
+            } else {
+                System.out.println("Error! No command.");
+            }
+            if (scanner.hasNextInt()) {
+                int value = scanner.nextInt();
+            } else {
+                System.out.println("Error! No number!.");
+            }
+
+            scanner.close();
+        }
+    }
+}
