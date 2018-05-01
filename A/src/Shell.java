896b579cab1ac6582c5d39f113b2742f77884f38
Martin Binder <martin.binder3@gmx.net>
2018-05-01 13:08:11 +0200

    work on shell input

--- A/src/Shell.java
+++ A/src/Shell.java
@@ -7,33 +7,86 @@ public final class Shell {
     private Shell() {
     }
 
-    private static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         execute(stdin);
     }
 
     private static void execute(BufferedReader stdin) throws IOException {
         boolean quit = false;
+
         while (!quit) {
-            System.out.println("prompt");
+            System.out.println("gol> ");
             String input = stdin.readLine();
             if (input == null) {
                 break;
             }
-            Scanner scanner = new Scanner(input);
-            scanner.useDelimiter("\\s+");
-            if (scanner.hasNext()) {
-                String command = scanner.next();
-            } else {
-                System.out.println("Error! No command.");
-            }
-            if (scanner.hasNextInt()) {
-                int value = scanner.nextInt();
-            } else {
-                System.out.println("Error! No number!.");
-            }
+            String[] tokens = input.trim().split("\\s+");
+            if (checkInput(tokens)) {
 
-            scanner.close();
+                switch (input.toLowerCase().charAt(0)) {
+                    case 'n':
+                        ;
+                        break;
+                    case 'a':
+                        ;
+                        break;
+                    case 'd':
+                        ;
+                        break;
+                    case 'g':
+                        ;
+                        break;
+                    case 'p':
+                        ;
+                        break;
+                    case 'c':
+                        ;
+                        break;
+                    case 'r':
+                        break;
+                    case 's':
+                        break;
+                    case 'h':
+                        break;
+                    case 'q':
+                        quit = true;
+                        break;
+                    default:
+                        error("Not a valid command!");
+                }
+
+
+            }
         }
+    }
+
+    private static void error(String msg) {
+        System.out.println("Error! " + msg);
+    }
+
+    private static boolean checkInput(String[] tokens) {
+        if (tokens.length > 3 || tokens.length == 0) {
+            error("Input has not the correct syntax. Try 'Help'.");
+            return false;
+        } else if (tokens.length == 3 && !(isANumber(tokens[1]) && isANumber(tokens[2]))) {
+            error("Input has not the correct syntax. Command + number + number expected.");
+            return false;
+        } else if (tokens.length == 2 && !isANumber(tokens[1])) {
+            error("Input has not the correct syntax. Command + number expected.");
+            return false;
+        } else if (tokens.length == 1)
+        return true;
+    }
+
+    private static boolean isANumber(String number) {
+        try {
+            Integer.parseInt(number);
+
+        } catch (NumberFormatException e) {
+            error("Not a number!");
+            return false;
+        }
+        return true;
     }
 }
