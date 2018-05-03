6f35cc6ee596078e41884a8fb1fa22499f8a618c
Martin Binder <martin.binder3@gmx.net>
2018-05-03 23:17:14 +0200

    important to prior revision! adds count for alive neighbors to every cell

--- A/src/Cell.java
+++ A/src/Cell.java
@@ -2,6 +2,7 @@ public class Cell {
     private boolean alive;
     private int column;
     private int row;
+    private int neighbors;
 
     public int getRow() {
         return row;
@@ -23,5 +24,13 @@ public class Cell {
 
     public void setAlive(boolean alive) {
         this.alive = alive;
+    }
+
+    public int getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(int neighbors) {
+        this.neighbors = neighbors;
     }
 }
