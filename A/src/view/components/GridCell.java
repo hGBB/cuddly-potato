0262cfba09771b9ada0d102aea5e579aa4cecacd
orax <binder34@gw.uni-passau.de>
2018-06-28 14:41:57 +0200

    clean up GridCell

--- A/src/view/components/GridCell.java
+++ A/src/view/components/GridCell.java
@@ -2,8 +2,6 @@ package view.components;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 
 public class GridCell extends JPanel {
